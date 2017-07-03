4ec5a2e96a1f5b3d9fa3e62c352ddbdf54447977
Abby Starnes <abbymstarnes@gmail.com>
2017-07-03 10:29:14 -0500

    v1 working

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const express = require('express');
 const app = express();
 // server can accept argument specifiying a custom port (e.g. for heroku)
 const port = process.env.PORT || 3000;
-// organize routes into multiple files
-const routes = require('route');
 // utilities for working with file & directory paths
 const path = require ('path');
 // parse incoming request bodies under req.body property
@@ -17,41 +15,6 @@ const bodyParser = require('body-parser');
 const fs = require ('fs');
 // seeded random number generator for JS
 const seedrandom = require('seedrandom');
-
-
-
-/*
-get monster/:id
-  // generate file based on seed
-  // number 1-9, letters a-z undercase, letters a-z uppercase, spaces, punctuation, length
-  first letter, length, last letter >> math.random
-
-  take seed, use to pick a body, eyes, and mouth
-  read body, eyes, mouth
-  compile body, eyes, mouth into one file
-  return compiled file
-  test monster/:id route
-*/
-
-// trying out seed random
-var rng = seedrandom('asfdf');
-var sng = seedrandom('Aasdf ');
-var tng = seedrandom('Aasdf  ');
-let answer1 = (rng()*10).toFixed(1);
-let answer2 = (sng()*10).toFixed(1);
-let answer3 = (tng()*10).toFixed(1);
-// var ddklj = seedrandom(answer2);
-// console.log((ddklj()*10).toFixed(2));
-console.log(answer1, answer2, answer3);
-
-let header = fs.readFileSync(__dirname + '/public/images/header.svg', 'UTF-8');
-let footer = fs.readFileSync(__dirname + '/public/images/footer.svg', 'UTF-8');
-// let body
-// let eyes
-// let mouth
-let bodyName = 'body';
-let eyesName = 'eyes';
-let mouthName = 'mouth';
 
 let pickAPart = function(answer, name){
   let part;
@@ -67,37 +30,10 @@ let pickAPart = function(answer, name){
   return part;
 }
 
-let body = pickAPart(answer1, bodyName);
-let eyes = pickAPart(answer2, eyesName);
-let mouth = pickAPart(answer3, mouthName);
-let creature = header + body + eyes + mouth + footer;
-fs.writeFileSync(__dirname + '/public/images/creature_sample.svg', creature)
-// console.log(body, 'after');
-
-
-// use seed random on answer of previous. reliable, random?
-
-// 3 options
-// 0? 1 2 3 4 5 6 7 8 9 10
-
-
-//header
-//body
-//eyes
-//mouth
-//footer
 
 // serves static files from public directory
 app.use(express.static(__dirname + '/public'));
 
-// var svg = fs.readFileSync(__dirname + '/public/images/body2.svg', 'UTF-8');
-// var eyes = fs.readFileSync(__dirname + '/public/images/eyes2.svg', 'UTF-8');
-// let combo = svg + eyes;
-// fs.writeFileSync(__dirname + '/public/images/combo_sample.svg', combo)
-// console.log(svg, 'svg');
-// console.log(eyes, 'eyes');
-// console.log(combo, 'combo');
-// set body-parser to parse JSON
 app.use(bodyParser.json());
 // parses urlencoded UTF-8 encoded body
 app.use(bodyParser.urlencoded({
@@ -111,6 +47,32 @@ app.use(bodyParser.urlencoded({
 app.get('/', function(req, res) {
   console.log('here');
   res.end('here');
+});
+
+// creature generator
+app.get('/creature/:seed', function(req, res) {
+  let seed = req.params.seed;
+  console.log(seed, 'seed');
+  // trying out seed random
+  var rng = seedrandom(seed);
+  var sng = seedrandom(seed + ' ');
+  var tng = seedrandom(seed + '  ');
+  let answer1 = (rng()*10).toFixed(1);
+  let answer2 = (sng()*10).toFixed(1);
+  let answer3 = (tng()*10).toFixed(1);
+
+  let header = fs.readFileSync(__dirname + '/public/images/header.svg', 'UTF-8');
+  let footer = fs.readFileSync(__dirname + '/public/images/footer.svg', 'UTF-8');
+  let bodyName = 'body';
+  let eyesName = 'eyes';
+  let mouthName = 'mouth';
+
+  let body = pickAPart(answer1, bodyName);
+  let eyes = pickAPart(answer2, eyesName);
+  let mouth = pickAPart(answer3, mouthName);
+  let creature = header + body + eyes + mouth + footer;
+  fs.writeFileSync(__dirname + '/public/images/creature_sample.svg', creature)
+  res.end(creature);
 });
 
 // if request hasn't been sent by end of app, send a 500 error and end the request
